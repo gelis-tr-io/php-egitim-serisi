719269e38f5998ce2290fdbb6b7841d6c48392e2
Burak Hurata <40127411+hurata@users.noreply.github.com>
2019-04-14 00:05:51 +0300

    Create header.php

---
+++ bilgim-var-proje-olustururken-kafam-karisiyor/dosya-yapisi/templates/constants/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <base href="<?php echo $URL; ?>">
+    <title>Dosya  yapısı</title>
+    <link rel="stylesheet" type="text/css" href="asset/css/main.css">
+</head>
+<body>
